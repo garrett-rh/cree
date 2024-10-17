3ecff0c1c8600fb7ad157127836af6bd2179d06e
Garrett <garretth@tuta.io>
2024-10-17 11:46:37 -0400

    now with arg flags

--- tree.c
+++ tree.c
@@ -1,5 +1,7 @@
 #include <dirent.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 
@@ -13,7 +15,7 @@ static int isDirectory(const char *fullPath) {
   return 1;
 }
 
-static void getFiles(char path[4096], char prefix[1024]) {
+static void getFiles(char path[4096], char prefix[1024], int depth, int files) {
   DIR *d;
   struct dirent *dir;
   char fullPath[4096];
@@ -22,6 +24,10 @@ static void getFiles(char path[4096], char prefix[1024]) {
   d = opendir(path);
   if (d) {
     while ((dir = readdir(d)) != NULL) {
+      if (depth == 0) {
+        break;
+      }
+
       if ((strcmp(dir->d_name, ".") == 0) || (strcmp(dir->d_name, "..") == 0)) {
         continue;
       }
@@ -30,8 +36,10 @@ static void getFiles(char path[4096], char prefix[1024]) {
       if (isDirectory(fullPath)) {
         printf("%s├── %s\n", prefix, dir->d_name);
         snprintf(newPrefix, sizeof(newPrefix), "%s│  ", prefix);
-        getFiles(fullPath, newPrefix);
-      } else {
+        depth--;
+        getFiles(fullPath, newPrefix, depth, files);
+        depth++;
+      } else if (files) {
         printf("%s├── %s\n", prefix, dir->d_name);
       }
     }
@@ -41,11 +49,41 @@ static void getFiles(char path[4096], char prefix[1024]) {
 
 int main(int argc, char **argv) {
   char dumb[1024] = "";
-  if (argc < 2) {
-    return 1;
+  char path[4096];
+  int files = 0;
+  int depth = -100;
+  int option;
+
+  while ((option = getopt(argc, argv, ":p:fd:h")) != -1) {
+    switch (option) {
+      case 'p':
+        snprintf(path, sizeof(path), "%s", optarg);
+        break;
+      case 'f':
+        files = 1;
+        break;
+      case 'd':
+        depth = atoi(optarg);
+        break;
+      case 'h':
+        printf("tree\n");
+        printf("-p <path>\ttakes the path to run tree on\n");
+        printf("-d <depth>\ttakes an int as the upper depth\n");
+        printf(
+            "-f\t set this variable if you want to see the directories & "
+            "files\n");
+        printf("-h\t print this page\n");
+        break;
+      case ':':
+        printf("one of the specified options needs a value\n");
+        break;
+      case '?':
+        printf("unknown option: %c\n", option);
+        break;
+    }
   }
 
-  getFiles(argv[1], dumb);
+  getFiles(path, dumb, depth, files);
 
   return 0;
 }
